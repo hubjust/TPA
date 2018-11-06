93619f17a4c97588dbaa09fe10b2d0022a5286cf
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2018-11-06 10:30:57 +0100

    XMLSerialization and tuple added

---
+++ Model/Tupl.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Model
+{
+    // Tuple (krotka) to uporządkowany ciąg, niezmienny, stałej wielkości i różnorodnych obiektów, np. każdy obiekt określonego typu.
+    // Krotka pozwala na szybkie grupowanie wielu wartości w jeden rezultat.
+
+    public class Tupl<T1, T2, T3>
+    {
+        public T1 Item1 { get; set; }
+        public T2 Item2 { get; set; }
+        public T3 Item3 { get; set; }
+
+        public Tupl() { }
+
+        public static implicit operator Tupl<T1, T2, T3>(Tuple<T1, T2, T3> t)
+        {
+            return new Tupl<T1, T2, T3>()
+            {
+                Item1 = t.Item1,
+                Item2 = t.Item2,
+                Item3 = t.Item3
+            };
+        }
+
+        public static implicit operator Tuple<T1, T2, T3>(Tupl<T1, T2, T3> t)
+        {
+            return Tuple.Create(t.Item1, t.Item2, t.Item3);
+        }
+
+
+    }
+}
---
+++ ViewModel/XML/XMLSerialize.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace ViewModel.XML
+{
+    public static class XmlSerialize
+    {
+        public static void XmlSerializer<T>(T source, string path, FileMode mode, string stylesheetName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T));
+            XmlWriterSettings settings = new XmlWriterSettings
+            {
+                Indent = true,
+                IndentChars = " ",
+                NewLineChars = "\r\n"
+            };
+
+            using (FileStream stream = new FileStream(path, mode, FileAccess.Write))
+            using (XmlWriter writer = XmlWriter.Create(stream, settings))
+            {
+                writer.WriteProcessingInstruction("xml-stylesheet", "type=\"text/xs1\" " + String.Format("href=\"{0}\"", stylesheetName));
+                serializer.Serialize(writer, source);
+            }
+        }
+    }
+}
