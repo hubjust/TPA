d193c267f18a57e5cc44f4bedc87cadafa84708a
Karol Podlewski <karol.podlewski@hotmail.com>
2018-11-07 10:21:27 +0100

    Gui Changes
    Now it can choose file and show file path!

---
+++ ViewModel/VMViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows.Forms;
+using System.Windows.Input;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+using Model;
+using ViewModel.ViewModelMetadata;
+
+namespace ViewModel
+{
+    public class VMViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand Click_Open { get; }
+        public string PathVariable { get; set; }
+
+        private AssemblyMetadata assemblyMetadata;
+        private ViewModelAssemblyMetadata viewModelAssemblyMetadata;
+
+        public ObservableCollection<ITreeViewItem> HierarchicalAreas { get; set; }
+
+
+        public VMViewModel()
+        {
+            HierarchicalAreas = new ObservableCollection<ITreeViewItem>();
+            Click_Open = new Command(Open);
+        }
+
+        private void Open()
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog
+            {
+                Filter = "Dynamic Library File(*.dll) | *.dll|"
+                       + "All files (*.*)| *.*"
+            };
+
+            fileDialog.ShowDialog();
+
+            if (fileDialog.FileName.Length == 0)     
+                MessageBox.Show("No files selected");
+            
+            else
+            {
+                PathVariable = fileDialog.FileName;
+                RaisePropertyChanged("PathVariable");
+            }
+        }
+
+        public virtual void RaisePropertyChanged(string propertyNam)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyNam));
+        }
+    }
+}
