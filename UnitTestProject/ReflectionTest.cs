4e999c451cab689f90e2f132437ff0e1fe1ddff8
Justyna Hubert <37309986+hubjust@users.noreply.github.com>
2018-11-07 11:22:35 +0100

    Tests for reflection.

--- UnitTestProject/ReflectionTest.cs
+++ UnitTestProject/ReflectionTest.cs
@@ -1,14 +1,63 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Model;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace UnitTestProject
 {
     [TestClass]
     public class ReflectionTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Assembly assembly;
+
+        [TestInitialize]
+        public void Init()
         {
+            string str = @"..\..\File\Model.dll";
+            assembly = Assembly.LoadFrom(str);
+        }
+
+        [TestMethod]
+        public void LoadingAssemblyTest()
+        {
+            Assert.IsTrue(assembly != null);
+        }
+
+        [TestMethod]
+        public void AssemblyMetadataShouldNotBeNullTest()
+        {
+            AssemblyMetadata assemblyMetadata = new AssemblyMetadata();
+            Assert.IsTrue(assemblyMetadata != null);
+        }
+
+        [TestMethod]
+        public void NamespacesInAssemblyMetadataTest()
+        {
+            AssemblyMetadata assemblyMetadata = new AssemblyMetadata(assembly);
+
+            List<string> namespaceNames = new List<string>(from Namespaces in assemblyMetadata.Namespaces
+                                                           select Namespaces.Name);
+
+            Assert.IsTrue(namespaceNames.Contains("Model"));
+        }
+
+        [TestMethod]
+        public void EmitPropertiesTest()
+        {
+            Type type = typeof(ExampleClass);
+            List<PropertyMetadata> properties = PropertyMetadata.EmitProperties(type.GetProperties()).ToList<PropertyMetadata>();
+
+            Assert.AreEqual(1, properties.Count);
+        }
+
+        [TestMethod]
+        public void TypesInMetadata() // Nie działa na razie
+        {
+            AssemblyMetadata assemblyMeta = new AssemblyMetadata(assembly);
+
+            //NamespaceMetadata namespaceMetadata = assemblyMeta.Namespaces.ToList<NamespaceMetadata>()[1];
         }
     }
 }
