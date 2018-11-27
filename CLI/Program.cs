3d00d5578aa5d9721edc2de64b584598fb6788bb
Karol Podlewski <karol.podlewski@hotmail.com>
2018-11-27 19:51:06 +0100

    ConsoleView (now CLI) update! Now it dont suck dick, and use VMViewModel

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
+using ViewModel;
 
 namespace CLI
 {
@@ -10,7 +10,83 @@ namespace CLI
     {
         static void Main(string[] args)
         {
+            VMViewModel dataContext = new VMViewModel(new CLIFileSelector());
+            dataContext.OpenDLL.Execute(null);
 
+            Stack<TreeViewItem> stack = new Stack<TreeViewItem>();
+            TreeViewItem current = dataContext.HierarchicalAreas[0];
+            current.IsExpanded = true;
+
+            string command;
+            string errorMessage = null;
+            bool showHelp = true;
+
+            while (true)
+            {
+                SetUpConsole(current.Name, stack.Count, showHelp, errorMessage);
+                errorMessage = null;
+                showHelp = false;
+                foreach (TreeViewItem item in current.Children)
+                    Console.WriteLine("\t" + item);
+                Console.Write("\n:: TYPE COMMAND ::\n:> ");
+
+                command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "help":
+                        showHelp = true;
+                        break;
+
+                    case "exit":
+                        return;
+
+                    case "back":
+                        if (stack.Count == 0)
+                            errorMessage = "You are on root!";
+                        else
+                            current = stack.Pop();
+                        break;
+
+                    default:
+                        stack.Push(current);
+                        try
+                        {
+                            current = current.Children.First(i => i.Name.Equals(command));
+                        }
+                        catch (InvalidOperationException)
+                        { errorMessage = "There is no such type!"; }
+                        break;
+                }
+
+                current.IsExpanded = true;
+            }
+
+        }
+
+        private static void SetUpConsole(string currentName, int size, bool help, string errorMessage)
+        {
+            Console.Clear();
+
+            if(!errorMessage.IsNullOrEmpty())
+            {
+                Console.WriteLine(":# ERROR: " + errorMessage + "\n");
+                help = true;
+            }
+
+            if(help)
+            {
+                string back = "\n   ~  back  - u can go back to preveius branch {" + size + "} times";
+                if (size == 0)
+                    back = "\n   ~  back  - u can't go back now, you are on root";
+
+                Console.WriteLine(":: AVALIABLE COMMANDS ::" +
+                                  "\n   ~ [type] - specify type to expand" +
+                                  back + "\n   ~  exit  - close program" +
+                                  "\n   ~  help  - show help\n" );
+            }
+
+            Console.WriteLine(":: CURRENT TREE ::\n" + currentName);
         }
     }
 }
