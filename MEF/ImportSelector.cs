7617a3cc8627c4340c63ff4f6826fce83df34ecc
Karol Podlewski <karol.podlewski@hotmail.com>
2019-01-02 15:33:01 +0100

    First MEF try

---
+++ MEF/ImportSelector.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+
+namespace MEF
+{
+    public class ImportSelector<T> : List<T>
+    {
+        public T GetImport()
+        {
+            var settings = ConfigurationManager.AppSettings;
+            string name = typeof(T).Name;
+            string result = settings[name];
+            return Find(item => item.GetType().Name == result);
+        }
+    }
+}
